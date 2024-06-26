5c7cef42488479cba6532c874d1f0381d1d8b594
DESKTOP-O1PRTS9\DELL <marcolloko15@gmail.com>
2024-06-26 15:13:31 +0000

    'first'

---
+++ marc.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>page de presentation</title>
+ <style>
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+
+        }
+        th td {
+            padding: 15px;
+            text-align: center;  /* Centrer le texte horizontalement */
+            vertical-align: middle; /* Centrer le texte verticalement */
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+        
+        th, td {
+            padding: 50px;
+            text-align: center;
+        }
+
+        
+        }
+
+        
+ </style>
+</head>
+<body>
+<table border="3">
+<center><h1> MARC EST LE BOSS</h1></center>
+<tr>
+<th>Exercice 1</th> 
+<th>Resultat</th>
+</tr>
+<tr>
+<td></td>
+<td></td>
+</tr>
+
+
+</table>
+</body>
+</html>
